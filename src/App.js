e6ee183f7f1c1762ad275498aa2a8470302ca478
Powerdby <hsjang3578@gmail.com>
2022-09-28 19:07:54 +0900

    220928

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+function Food({name,picture, rating}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>먹고싶은 ({name})</h3>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt= {name} />
     </div>
   );
 }
 
+const foodILike = [
+  {
+    id: 1,
+    name: '김치',
+    image: 'https://image.kmib.co.kr/online_image/2021/0313/611721110015626599_1.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: '삼기업살',
+    image: 'https://img7.yna.co.kr/photo/yna/YH/2017/04/23/PYH2017042315750001300_P2.jpg',
+    rating: 4.9,
+  },
+]; //food_i_like
+
+function App() {
+  return ( 
+  <div>
+    <h1>야미</h1>
+    {foodILike.map( dish => (<Food key={dish.id} name = {dish.name} picture = {dish.image} rating={dish.rating}/>))}
+  </div>
+  );
+    
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+}
 export default App;
+
